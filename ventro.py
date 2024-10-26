6505f04aa378e1a76f91bcdd6d7e50dec18806d6
SilumanKatak <142733715+silumankatak@users.noreply.github.com>
2024-10-27 00:09:29 +0700

    Update ventro.py

--- ventro.py
+++ ventro.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 from discord.ext.commands import has_any_role  # Pastikan has_any_role diimpor dengan benar
 import asyncio
 import logging
+import os
 
 TOKEN = os.getenv('MTI5OTM5Njg1MTYyMzEzMzI3NA.GXwhbA.CyCPbmuLaQAxo4BIzDThpcc3ka9gTXIyurW8nk')
 
